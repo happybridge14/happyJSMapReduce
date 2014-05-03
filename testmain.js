90fb27f78f028c51589b668d082af57a15228aa6
happybridge14 <happybridge14@163.com>
2014-05-04 01:18:56 +0800

    remove whitespace

--- testmain.js
+++ testmain.js
@@ -1,8 +1,8 @@
-require.config({　　　　
-    paths: {　　　　　　
+require.config({
+    paths: {
         "underscore": "components/underscore/underscore",
-        "mapreduce": "mapreduce"　　　　
-    }　　
+        "mapreduce": "mapreduce"　　　
+    }
 });
 
 require(["mapreduce"],
