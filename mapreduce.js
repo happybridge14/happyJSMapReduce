3a0b4963ecbd5305f47717483a67e417e1068a1f
happybridge14 <happybridge14@163.com>
2014-05-04 01:14:15 +0800

    Pass test

--- mapreduce.js
+++ mapreduce.js
@@ -1,36 +1,36 @@
 define("mapreduce", ["underscore"],
-		function (_){
-			return function(raw, filter, map, reduce, reduceRaw) {
-				console.log("aaaaaaa")
-				var i;
-				var tmp;
-				if (_.isFunction(filter)) {
-					raw = filter(raw);
-				} else {
-					if (_.isArray(raw)) {
-						raw = [raw]
-					} else if (_.isString(raw) && _.isString(filter)) {
-						raw = raw.split(filter);
-					} else if (_.isObject(raw)) {
-						tmp = raw;
-						raw = [];
-						for (i in tmp) {
-							raw.push({key:i, value:tmp[i]});
-						}
-					} else {
-						return raw;
-					}
-				}
+    function(_) {
+        return function(raw, filter, map, reduce, reduceRaw) {
+            var i;
+            var tmp;
+            if (_.isFunction(filter)) {
+                raw = filter(raw);
+            } else {
+                if (_.isArray(raw)) {
+                    raw = [raw];
+                } else if (_.isString(raw) && _.isString(filter)) {
+                    raw = raw.split(filter);
+                } else if (_.isObject(raw)) {
+                    tmp = raw;
+                    raw = [];
+                    for (i in tmp) {
+                        raw.push({
+                            key: i,
+                            value: tmp[i]
+                        });
+                    }
+                } else {
+                    return raw;
+                }
+            }
 
-				var length = raw.length;
-				console.log("hello: " + raw);
-				tmp = raw;
-				raw = [];
-				for (i = 0; i < length; i++) {
-					raw.concat(_.map(tmp, map));
-				};
-				console.log("hello: " + raw);
-				return _.reduce(raw, reduce, reduceRaw);
-			}
-		}
-	);
+            var length = raw.length;
+            tmp = raw;
+            raw = [];
+            for (i = 0; i < length; i++) {
+                raw = raw.concat(_.map(tmp[i], map, this));
+            };
+            return _.reduce(raw, reduce, reduceRaw);
+        }
+    }
+);
---
+++ testmain.js
@@ -0,0 +1,37 @@
+require.config({　　　　
+    paths: {　　　　　　
+        "underscore": "components/underscore/underscore",
+        "mapreduce": "mapreduce"　　　　
+    }　　
+});
+
+require(["mapreduce"],
+    function(mapreduce) {
+        var raw = ["aaa", "bbb", "aaa", "ccc"];
+
+        var filter = function(raw) {
+
+        }
+
+        var map = function(value) {
+            return {
+                key: value,
+                value: 1
+            };
+        }
+
+        var reduce = function(mem, value) {
+            if (!mem[value["key"]]) {
+                mem[value["key"]] = 0;
+            }
+            mem[value["key"]] = mem[value["key"]] + value["value"];
+            return mem;
+        }
+
+        var reduceRaw = {};
+
+        var result = mapreduce(raw, "", map, reduce, reduceRaw);
+
+        console.log(result);
+    }
+);
